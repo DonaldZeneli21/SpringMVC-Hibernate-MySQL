d3b4ec68d2db9799c74244fd2cf55f3575a9f0b2
dzeneli <dzeneli@PC-DZENELI.ikub.local>
2020-03-05 09:56:39 +0100

    Commit

--- MyProject/src/main/java/com/myproject/controller/DestinationController.java
+++ MyProject/src/main/java/com/myproject/controller/DestinationController.java
@@ -40,6 +40,7 @@ public class DestinationController {
 		return ResponseEntity.ok(destination);
 	}
 
+	
 	/* add new destination */
 	@PostMapping("/addDestination")
 	public ResponseEntity<?> save(@RequestBody Destination destination) {
